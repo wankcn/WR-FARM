243a83295f260db3f26964a72a18d5a879b70ef5
wankcn <wankcn@icloud.com>
2022-09-27 04:07:30 +0800

    制作ItemDetailsList_SO

---
+++ Assets/Scripts/Utilities/DataCollection.cs
@@ -0,0 +1,63 @@
+using System;
+using UnityEngine;
+
+/**
+ * 所有的自定义类，结构体
+ */
+[Serializable]
+public class ItemDetails
+{
+    /// <summary>
+    /// 物品ID
+    /// </summary>
+    public int itemID;
+
+    public string name;
+
+    public Sprite itemIcon;
+
+    /// <summary>
+    /// 物品类型，枚举 种子，可交易，或者家具等
+    /// </summary>
+    public ItemType itemType;
+
+    /// <summary>
+    /// 在世界地图上产生时显示的图片，区别于icon没有描边
+    /// </summary>
+    public Sprite itemOnWorldSprite;
+
+    /// <summary>
+    /// 详情
+    /// </summary>
+    public string itemDescription;
+
+    /// <summary>
+    /// 上下左右与斜方向可以在多少网格范围内使用
+    /// </summary>
+    public int itemUseRadius;
+
+    /// <summary>
+    /// 是否可以被拾取
+    /// </summary>
+    public bool canPickedup;
+
+    /// <summary>
+    /// 是否可丢弃
+    /// </summary>
+    public bool canDropped;
+
+    /// <summary>
+    /// 是否可举起
+    /// </summary>
+    public bool canCarried;
+
+    /// <summary>
+    /// 物品价格
+    /// </summary>
+    public int itemPrice;
+
+    /// <summary>
+    /// 表示物品可售卖时折扣的百分比
+    /// </summary>
+    [Range(0, 1)] public float sellPercentage;
+}
---
+++ Assets/Scripts/Utilities/Enums.cs
@@ -0,0 +1,17 @@
+/**
+ * 存储所有枚举变量
+ */
+public enum ItemType
+{
+    Seed, // 种子
+    Commodity, // 商品
+    Furniture, // 家具
+
+    HoeTool, // 锄头
+    ChopTool, // 砍树的
+    BreakTool, // 砸石头
+    ReapTool, // 割草
+    WaterTool, // 浇水
+    CollectTool, // 收集
+    ReapableScenery,// 可被收割的杂草
+}
